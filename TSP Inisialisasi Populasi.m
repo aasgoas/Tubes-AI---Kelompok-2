7336887b561a3cd0b84044b2164bbe9880af92d8
Aas <95178274+aasgoas@users.noreply.github.com>
2022-07-06 11:13:31 +0700

    Update TSP Inisialisasi Populasi.m

--- TSP Inisialisasi Populasi.m
+++ TSP Inisialisasi Populasi.m
@@ -1,14 +1,13 @@
-%%Membangkitkan ukran populasi kromosom masing-masing kromosom 
-berisi
-%%bilangan 1 sampai JumGen(jumlah kota)
-%%
-%%Input:
-%%UkPop : Ukuran populasi atau jumlah kromosom dalm populasi
-%%JumGen: jumlah gen(jumlah kota)
-%%
-%%Output:
-%%Pupulasi : kumpulan kromosom matriks berukuran UkPop x JumGen
-%%
+## Membangkitkan ukran populasi kromosom masing-masing kromosom berisi
+## bilangan 1 sampai JumGen(jumlah kota)
+
+## Input:
+## UkPop : Ukuran populasi atau jumlah kromosom dalm populasi
+## JumGen: jumlah gen(jumlah kota)
+
+## Output:
+## Pupulasi : kumpulan kromosom matriks berukuran UkPop x JumGen
+
 function Populasi = TSPInisialisasiPopulasi(UkPop,JumGen)
 for ii=1:UkPop,
  [Xval,Ind]= sort(rand(1, JumGen));
